89c79e6b9fe43b8b155f4fd0fd1ad918acd4fd61
martamorg <marta.moreno@etudiant.univ-rennes1.fr>
2023-01-19 12:57:18 +0100

    configs for adding test slice

--- open5gs/config/5gc_up_init.sh
+++ open5gs/config/5gc_up_init.sh
@@ -11,7 +11,7 @@ elif [[ "$COMPONENT_NAME" =~ ^upf_cld$ ]]; then
 
     iperf3 -B 10.45.0.1 -s -fm &
 
-    cp /open5gs/install/etc/open5gs/temp/upf_cld.yaml /open5gs/install/etc/open5gs/upf.yaml 
+    cp /open5gs/install/etc/open5gs/temp/upf_cld.yaml /open5gs/install/etc/open5gs/upf.yaml
 
 elif [[ "$COMPONENT_NAME" =~ ^upf_mec$ ]]; then
     ip tuntap add name ogstun mode tun
@@ -21,7 +21,17 @@ elif [[ "$COMPONENT_NAME" =~ ^upf_mec$ ]]; then
 
     iperf3 -B 10.46.0.1 -s -fm &
 
-    cp /open5gs/install/etc/open5gs/temp/upf_mec.yaml  /open5gs/install/etc/open5gs/upf.yaml 
+    cp /open5gs/install/etc/open5gs/temp/upf_mec.yaml  /open5gs/install/etc/open5gs/upf.yaml
+
+elif [[ "$COMPONENT_NAME" =~ ^upf_test$ ]]; then
+    ip tuntap add name ogstun mode tun
+    ip addr add 10.47.0.1/16 dev ogstun
+    ip link set ogstun up
+    iptables -t nat -A POSTROUTING -s 10.47.0.1/16 ! -o ogstun -j MASQUERADE
+
+    iperf3 -B 10.47.0.1 -s -fm &
+
+    cp /open5gs/install/etc/open5gs/temp/upf_test.yaml  /open5gs/install/etc/open5gs/upf.yaml
 
 else
 	echo "Error: Invalid component name: '$COMPONENT_NAME'"
